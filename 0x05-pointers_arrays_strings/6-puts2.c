9b998dc23d7d08f77cd1bc152b09416161c6fb46
UstazJizzy <laijizzy@gmail.com>
2023-02-27 17:04:28 -0800

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,12 +17,12 @@ void puts2(char *str)
 		y++;
 		longi++;
 	}
-	t = longi = 1;
+	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
 	{
-		_putchar(str[0]);
+		_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
